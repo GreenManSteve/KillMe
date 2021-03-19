981b4e57ab4b5e6be941a865c3fa6fea06dc407f
Steven Attwell <stevenattwell@hotmail.com>
2021-03-19 02:24:36 +0000

    Please work

--- tests/test_handler.py
+++ tests/test_handler.py
@@ -1,3 +1,14 @@
-from .abs_patient import AbsPatient
-from .male import Male
-from .female import Female
+import unittest
+from patient.male import Male
+
+
+class TestHandlerCase(unittest.TestCase):
+
+    def _score_age(self):
+        male = Male(20, 20, False, 25, 25)
+        score = male.calculate_framingham()
+        self.assertEqual(male.score_risk, score)
+
+
+if __name__ == '__main__':
+    unittest.main
